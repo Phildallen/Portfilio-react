70bbda2298da343d3bfd3cebc247ba3957972f78
Phildallen <phil_372@live.com>
2023-07-26 14:57:43 +0100

    borders

---
+++ src/components/Layout/index.js
@@ -0,0 +1,18 @@
+import Header from "../Header";
+import Portfolio from "../Portfolio";
+import Sidebar from "../Sidebar";
+import Corner from "../TopLeftCorner";
+const Layout = ({ children }) => {
+    return (
+        <>
+
+            <div className="layoutWrapper">
+                <Corner />
+                <Header />
+                <Sidebar />
+                <Portfolio />
+            </div>
+        </>
+    );
+};
+export default Layout;
---
+++ src/components/TopLeftCorner/index.js
@@ -0,0 +1,9 @@
+const Corner = () => {
+    return (
+        <>
+            <div className="corner"></div>
+        </>
+    )
+}
+
+export default Corner
