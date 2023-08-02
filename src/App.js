32b929f0c18c83db0f8c5a234bb8e7184d202558
Phildallen <phil_372@live.com>
2023-08-02 17:33:37 +0100

    about me, bootcamp deats and contacts added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { faEnvelope} from '@fortawesome/free-solid-svg-icons'
+import {faLinkedin, faGithub} from '@fortawesome/free-brands-svg-icons'
 import './App.css';
 import "./components/css/index.css"
 import Header from "./components/Header";
 import Home from "./components/Home";
 import {useState} from "react";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+
+library.add(fab, faEnvelope, faLinkedin, faGithub)
 
 
 const App = () => {
